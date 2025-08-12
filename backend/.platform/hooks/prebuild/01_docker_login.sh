159a408b485421cf6903e77d29ec08efa95107fd
Bahae <bahaeddine_moutaouikil@um5.ac.ma>
2025-08-12 13:48:14 +0100

    add GHCR docker login hook for Elastic Beanstalk deployment

--- backend/.platform/hooks/prebuild/01_docker_login.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-echo "Logging into GHCR..."
-docker login "${DOCKER_REGISTRY_SERVER}" -u "${DOCKER_REGISTRY_USERNAME}" -p "${DOCKER_REGISTRY_PASSWORD}"
-echo "Logged into ${DOCKER_REGISTRY_SERVER}"
